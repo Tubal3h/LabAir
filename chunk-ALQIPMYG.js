cc9cde7973190025d92f601c913fdc6c46e1ee4a
Macwell <2bal3h@gmail.com>
2025-06-16 17:31:33 +0200

    Auto-generated commit

---
+++ chunk-ALQIPMYG.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-TTR5OSFT.js";import"./chunk-CYFMJ57Z.js";import"./chunk-H3KW6RZE.js";import"./chunk-QDANSVX3.js";export{a as SearchComponent};
--- chunk-MZQ2WHCG.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-CQE7WRDC.js";import"./chunk-LHJOZ67R.js";import"./chunk-NIGWU2LA.js";import"./chunk-SSAAJQVM.js";export{a as SearchComponent};
